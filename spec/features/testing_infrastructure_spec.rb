c083d138cd34aab142d56ac19b1e23561acfaa43
ollie beney <ombeney@gmail.com>
2020-09-16 15:36:20 +0100

    added attack page

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,20 @@
 feature "Testing infrastructure" do
-  scenario "Can run app and check page content" do
-    visit("/")
-    expect(page).to have_content "Testing infrastructure working!"
+  scenario 'can you add two names to battle?' do
+    sign_in_and_play
+    expect(page).to have_content "Player_1: Linus"
+    expect(page).to have_content "Player_2: Ollie"
   end
+
+  scenario "Can i see players hit points" do
+    sign_in_and_play
+    expect(page).to have_content "HP: 100"
+  end
+
+  scenario "I want to beat up linus" do
+    sign_in_and_play
+    click_button "player_2_attack"
+    expect(page).to have_content "Ollie attacked Linus"
+  end 
+
+
 end
